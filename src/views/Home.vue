f02acc6f20b58a7d51f8892ee2efac6a28f8a940
Kyle Bondo <gagglepod@gmail.om>
2022-03-21 14:59:29 -0500

    Baseline app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SingleProject from '../components/SingleProject.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: { SingleProject },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+    .then(res => res.json())
+    .then(data => this.projects = data)
+    .catch(err => console.log(err.message))
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project) => {
+        return project.id !== id
+      })
+    },
+    handleComplete(id) {
+      let p = this.projects.find(project => {
+        return project.id === id
+      })
+      p.complete = !p.complete
+    }
   }
 }
 </script>
